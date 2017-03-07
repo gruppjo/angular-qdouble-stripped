8e491f526d7d856ea83cbfc724fe67061a1b8ddc
Jonathan Grupp <jonathan.grupp@nolteundlauth.de>
2017-03-07 14:03:46 +0100

    #4 remove store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,25 +32,11 @@ import { IdlePreload, IdlePreloadModule } from '@angularclass/idle-preload';
 import { EffectsModule } from '@ngrx/effects';
 import { RouterStoreModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { useLogMonitor } from '@ngrx/store-log-monitor';
 
 import { routes } from './app.routing';
 import { rootReducer } from './reducers';
-import { StoreDevToolsModule } from './features/store-devtools.module';
 import { UserEffects } from './user/user.effects';
 
-const STORE_DEV_TOOLS_IMPORTS = [];
-if (ENV === 'development' && !AOT &&
-  ['monitor', 'both'].includes(STORE_DEV_TOOLS) // set in constants.js file in project root
-) STORE_DEV_TOOLS_IMPORTS.push(...[
-  StoreDevtoolsModule.instrumentStore({
-    monitor: useLogMonitor({
-      visible: true,
-      position: 'right'
-    })
-  })
-]);
 
 export const APP_IMPORTS = [
   EffectsModule.run(UserEffects),
@@ -59,8 +45,6 @@ export const APP_IMPORTS = [
   RouterModule.forRoot(routes, { useHash: false, preloadingStrategy: IdlePreload }),
   RouterStoreModule.connectRouter(),
   StoreModule.provideStore(rootReducer),
-  STORE_DEV_TOOLS_IMPORTS,
-  StoreDevToolsModule
 ];
 
 @NgModule({
