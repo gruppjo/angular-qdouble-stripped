7876c255c88c1a3624223ce393457f44a66d0f2d
Jonathan Grupp <jonathan.grupp@nolteundlauth.de>
2017-03-07 18:25:15 +0100

    #7 add jquery in lazy component

--- src/app/features/lazy/lazy.component.ts
+++ src/app/features/lazy/lazy.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'my-lazy',
   templateUrl: './lazy.component.html'
 })
 
-export class LazyComponent {}
+export class LazyComponent implements OnInit {
+  constructor (
+    private el: ElementRef
+  ) { }
+
+  ngOnInit() {
+    console.log($(this.el.nativeElement).width());
+  }
+
+}
